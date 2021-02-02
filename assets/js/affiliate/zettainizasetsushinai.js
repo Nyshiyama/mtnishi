ea07ac884c1173342bda118aa8c58e7cfa471c99
Nyshiyama <9534sdkmmmk@gmail.com>
2021-02-02 14:27:01 +0900

    アフィ用のjsファイルを追加

---
+++ assets/js/affiliate/zettainizasetsushinai.js
@@ -0,0 +1,7 @@
+(function(b,c,f,g,a,d,e){b.MoshimoAffiliateObject=a;
+  b[a]=b[a]||function(){arguments.currentScript=c.currentScript
+  ||c.scripts[c.scripts.length-2];(b[a].q=b[a].q||[]).push(arguments)};
+  c.getElementById(a)||(d=c.createElement(f),d.src=g,
+  d.id=a,e=c.getElementsByTagName("body")[0],e.appendChild(d))})
+  (window,document,"script","//dn.msmstatic.com/site/cardlink/bundle.js","msmaflink");
+  msmaflink({"n":"絶対に挫折しない　iPhoneアプリ開発「超」入門 第8版 【Xcode 11 ＆ iOS 13】 完全対応 [ 高橋 京介 ]","b":"","t":"","d":"https:\/\/thumbnail.image.rakuten.co.jp","c_p":"\/@0_mall\/book\/cabinet\/4622","p":["\/9784815604622.jpg","\/9784815604622_2.jpg","\/9784815604622_3.jpg"],"u":{"u":"https:\/\/item.rakuten.co.jp\/book\/16146237\/","t":"rakuten","r_v":""},"b_l":[{"id":1,"u_tx":"楽天市場で見る","u_bc":"#f76956","u_url":"https:\/\/item.rakuten.co.jp\/book\/16146237\/","a_id":2387921,"p_id":54,"pl_id":27059,"pc_id":54,"s_n":"rakuten","u_so":1}],"eid":"gt6FU","s":"s"});
